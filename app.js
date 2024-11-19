2949a3012f582369f53514224dba6c39d95016e9
jolyssa <jolyssa.armstrong@gmail.com>
2024-11-19 17:38:46 -0500

    first commit for storybooks

---
+++ app.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const connectDB = require('./config/db')
+
+//Load config file
+dotenv.config({ path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
+
+// Connect to our database
+connectDB()
+
+// App set to express()
+const app = express()
+
+//Logging
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// Template engine - Handlebars
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// PORT
+const PORT = process.env.PORT || 3000
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+
+// Listening for our PORT
+app.listen(
+    PORT,
+    console.log(`Server RUNNING 🏃🏾‍♀️ in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
