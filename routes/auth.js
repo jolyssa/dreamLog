de7891f9fef39193ddab7fee47e0a5965dd27c42
jolyssa <jolyssa.armstrong@gmail.com>
2024-11-20 23:58:05 -0500

    route for logout

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @ desc   Google auth callback
-// @route    GET /auth/google/callback
+// @route   GET /auth/google/callback
 router.get(
     '/google/callback',
      passport.authenticate('google', { failureRedirect: '/'}),
@@ -16,5 +16,13 @@ router.get(
     res.redirect('/dashboard')
     }
 )
+
+// @desc    Logout user
+// @route   /auth/logout
+
+router.get('/logout', (req,res) => {
+    req.logout()
+    res.redirect('/')
+})
 
 module.exports = router
