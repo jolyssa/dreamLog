1ad8c5d4ce8ec12fdd1d39f6f9384178f2b98f05
jolyssa <jolyssa.armstrong@gmail.com>
2024-11-22 22:55:40 -0500

    edit and delete button on public stories page, with routes working as well. put and delete requests added.

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
-
+ 
 //Load config file
 dotenv.config({ path: './config/config.env' })
 
@@ -25,12 +26,22 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
+
 //Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 // Handlebars
 app.engine(
@@ -40,8 +51,11 @@ app.engine(
             formatDate,
             stripTags,
             truncate,
+            editIcon,
+            select,
         },
-        defaultLayout: 'main', extname: '.hbs'
+        defaultLayout: 'main',
+        extname: '.hbs'
     })
 )
 app.set('view engine', '.hbs')
@@ -59,6 +73,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
